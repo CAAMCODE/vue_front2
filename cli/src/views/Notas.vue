a31fe4587f6869debaa6a4a3eafad61ceb1a7162
CAABCODE <ing.amaya.bautista@gmail.com>
2021-10-02 19:02:56 -0500

    proyecto de estudio para trabajar en yoparque

---
+++ cli/src/views/Notas.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h2>Notas</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Nota</th>
+          <th scope="col">Descripción</th>
+          <th scope="col">Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row">1</th>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  created() {},
+};
+
+methods: {
+  listarNotas();
+}
+</script>
+
+<style></style>
